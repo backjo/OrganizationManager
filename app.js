88ba484eb19d770249c104bb935c31d73a7749cc
Jonah Back <backjo@mail.uc.edu>
2014-10-01 17:32:31 -0400

    Editing events support

--- app.js
+++ app.js
@@ -161,6 +161,7 @@ app.get('/event/add',passportConf.isAuthenticated, eventController.addEvent);
 app.post('/event/add', passportConf.isAuthenticated, eventController.postEvent);
 app.get('/event/:id', eventController.getEvent);
 app.post('/event/:id', eventController.postUpdate);
+app.get('/event/:id/edit', passportConf.isAuthenticated, eventController.editEvent);
 app.del('/event/:id', passportConf.isAuthenticated, eventController.deleteEvent);
 app.get('/event/:id/:mnum/deny', passportConf.isAuthenticated, eventController.denyAttendance)
 app.get('/event/:id/:mnum', passportConf.isAuthenticated, eventController.postConfirmation);
